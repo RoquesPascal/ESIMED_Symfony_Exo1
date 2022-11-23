7ade27a36671ef5e93e12d1fce3d5dd824d350e8
pasca <pascal>
2022-11-23 18:22:28 +0100

    Symfonisation de la vue show

--- src/Controller/FlyController.php
+++ src/Controller/FlyController.php
@@ -6,16 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\FlyRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class FlyController extends AbstractController
 {
-    #[Route('/', name: 'app_fly')]
+    #[Route('/', name: 'index')]
     public function index(FlyRepository $flyRepository): Response
     {
         $listeVols = $flyRepository->findAll();
 
         return $this->render('fly/index.html.twig', [
             'listeVols' => $listeVols,
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'show')]
+    public function show(FlyRepository $flyRepository, Request $request): Response
+    {
+        $vol = $flyRepository->find($request->attributes->get('id'));
+
+        return $this->render('fly/show.html.twig', [
+            'vol' => $vol,
         ]);
     }
 }
